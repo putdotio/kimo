300bde62dfeff5df2a046d572789536ab3dc4958
Omer Murat Yildirim <omermuratyildirim@gmail.com>
2020-09-18 18:06:06 +0300

    no need to define config path

--- kimo-daemon-entrypoint.sh
+++ kimo-daemon-entrypoint.sh
@@ -3,4 +3,4 @@ echo "mysql sleep query..."
 mysql -u kimo -p123 -h tcpproxy -e "SELECT SLEEP(100000)" &
 
 echo "running kimo server..."
-/go/src/kimo/kimo -c /etc/kimo.toml daemon
+/go/src/kimo/kimo daemon
