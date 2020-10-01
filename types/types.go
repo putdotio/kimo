737766e7d3eb2432100a47e52d42dbe6cd9208ce
Omer Murat Yildirim <omermuratyildirim@gmail.com>
2020-10-01 13:00:47 +0300

    comments for exported types and methods

--- types/types.go
+++ types/types.go
@@ -3,15 +3,17 @@ package types
 import (
 	"database/sql"
 
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql" // imports mysql driver
 	gopsutilNet "github.com/shirou/gopsutil/net"
 )
 
+// Addr is an address type that is used to define a host
 type Addr struct {
 	Host string `json:"host"`
 	Port uint32 `json:"port"`
 }
 
+// DaemonProcess is the process type in terms of Daemon context
 type DaemonProcess struct {
 	Laddr    gopsutilNet.Addr `json:"localaddr"`
 	Status   string           `json:"status"`
@@ -21,6 +23,7 @@ type DaemonProcess struct {
 	CmdLine  []string         `json:"cmdline"`
 }
 
+// IsEmpty is used for determining whether a DaemonProcess has really a process information or not.
 func (dp *DaemonProcess) IsEmpty() bool {
 	if dp.Pid > 0 {
 		return false
@@ -28,6 +31,7 @@ func (dp *DaemonProcess) IsEmpty() bool {
 	return true
 }
 
+// MysqlProcess is the process type in terms of MySQL context (a row from processlist table)
 type MysqlProcess struct {
 	ID      int32          `json:"id"`
 	User    string         `json:"user"`
@@ -39,6 +43,7 @@ type MysqlProcess struct {
 	Address Addr           `json:"address"`
 }
 
+// TCPProxyRecord is type for defining a connection through TCP Proxy to MySQL
 type TCPProxyRecord struct {
 	ProxyInput   Addr
 	ProxyOutput  Addr
@@ -46,15 +51,12 @@ type TCPProxyRecord struct {
 	ClientOutput Addr
 }
 
-type KimoDaemonResponse struct {
-	Hostname        string          `json:"hostname"`
-	DaemonProcesses []DaemonProcess `json:"processes"`
-}
-
+// KimoServerResponse is type for returning a response from kimo server
 type KimoServerResponse struct {
 	ServerProcesses []ServerProcess `json:"processes"`
 }
 
+// ServerProcess is the final processes that is combined from DaemonProcess + TCPProxyRecord + MysqlProcess
 type ServerProcess struct {
 	ID        int32    `json:"id"`
 	MysqlUser string   `json:"mysql_user"`
@@ -65,5 +67,5 @@ type ServerProcess struct {
 	Info      string   `json:"info"`
 	CmdLine   []string `json:"cmdline"`
 	Pid       int32    `json:"pid"`
-	Host      string   `json:"host`
+	Host      string   `json:"host"`
 }
