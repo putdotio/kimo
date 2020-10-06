4d27bbbeaa329f7dc578be09f5047317f8495a0a
Omer Murat Yildirim <omermuratyildirim@gmail.com>
2020-10-06 11:48:48 +0300

    log

--- kimo-daemon-entrypoint.sh
+++ kimo-daemon-entrypoint.sh
@@ -2,5 +2,5 @@
 echo "mysql sleep query..."
 mysql -u kimo -p123 -h tcpproxy -e "SELECT SLEEP(100000)" &
 
-echo "running kimo server..."
-/go/src/kimo/kimo -d  daemon
+echo "running kimo daemon..."
+/go/src/kimo/kimo --debug  daemon
